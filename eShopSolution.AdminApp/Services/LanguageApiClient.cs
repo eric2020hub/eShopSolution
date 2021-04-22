3078ec83e6f30f9e53f04d39aee426748d8d3d3a
eric2020hub <erictrinh2289@gmail.com>
2021-04-21 17:38:54 -0700

    set language

---
+++ eShopSolution.AdminApp/Services/LanguageApiClient.cs
@@ -0,0 +1,35 @@
+﻿using eShopSolution.ViewModels.Common;
+using eShopSolution.ViewModels.System.Languages;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace eShopSolution.AdminApp.Services
+{
+    public class LanguageApiClient : BaseApiClient, ILanguageApiClient
+    {
+        //private readonly IHttpClientFactory _httpClientFactory;
+        //private readonly IConfiguration _configuration;
+        //private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public LanguageApiClient(IHttpClientFactory httpClientFactory, 
+            IConfiguration configuration, 
+            IHttpContextAccessor httpContextAccessor) 
+            : base(httpClientFactory, configuration, httpContextAccessor )
+        {
+            //_httpClientFactory = httpClientFactory;
+            //_configuration = configuration;
+            //_httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<ApiResult<List<LanguageVm>>> GetAll()
+        {
+            return await GetAsync<ApiResult<List<LanguageVm>>>("/api/languages");
+        }
+    }
+}
--- eShopSolution.AdminApp/Startup.cs
+++ eShopSolution.AdminApp/Startup.cs
@@ -45,6 +45,8 @@ namespace eShopSolution.AdminApp
             services.AddTransient<IHttpContextAccessor, HttpContextAccessor>();
             services.AddTransient<IUserApiClient, UserApiClient>();
             services.AddTransient<IRoleApiClient, RoleApiClient>();
+            services.AddTransient<ILanguageApiClient, LanguageApiClient>();
+
             IMvcBuilder builder = services.AddRazorPages();
             var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 #if DEBUG
